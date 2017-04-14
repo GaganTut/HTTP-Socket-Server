816285fa7d5a18482ebba4252f6c542f146ce63c
GaganTut <gstut@live.com>
2017-04-13 19:08:35 -1000

    Added error message if path name is not given

--- client.js
+++ client.js
@@ -1,9 +1,13 @@
 /*jshint esversion: 6*/
 const net = require('net');
-const path = process.argv[2] || '/';
+const path = process.argv[2];
 
 const client = net.connect({port: 8080}, () => {
-  client.write(`GET ${path} HTTP/1.1`);
+  if (path === undefined) {
+    process.stdout.write(`Type '/sample/sample' after client.js to run`);
+  } else {
+    client.write(`GET ${path} HTTP/1.1`);
+  }
 
   client.on('data', (data) => {
     const splitData = data.toString().split('\n\n');
