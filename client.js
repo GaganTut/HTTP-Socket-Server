c63b851970ca40227ef6549541fe036dc027dfcc
GaganTut <gstut@live.com>
2017-04-14 11:09:55 -1000

    Working on displaying non local sites

--- client.js
+++ client.js
@@ -2,27 +2,49 @@
 const net = require('net');
 const fullUrl = process.argv[2];
 
-const hostName = fullUrl.split(":")[0] || null;
-const portNum = fullUrl.split(":")[1].split("/")[0] || null;
-const pathName = `/${fullUrl.split("/")[1]}` || null;
-
 const responseHeaders = [];
-if (hostName === undefined) {
-  process.stdout.write(`Type path as such: "node client.js localhost:8080/sample"`);
-} else {
-  const client = net.connect({host: hostName, port: portNum}, () => {
-    if (pathName === undefined) {
-      process.stdout.write(`Type path as such: "node client.js localhost:8080/sample"`);
-    } else {
-      client.write(`GET ${pathName} HTTP/1.1
+
+const connectClient = (host, port, path) => {
+  console.log(host, port, path);
+  const client = net.connect({host:host, port: port}, () => {
+    client.write(`GET ${path} HTTP/1.1
 Date: ${new Date().toUTCString()}
 Host: ${client.remoteAddress}
 User-Agent: Yooooooo`);
-    }
-    client.on('data', (data) => {
-      const splitData = data.toString().split('\n\n');
-      responseHeaders.push(data.toString().split('\n\n')[0]);
-      process.stdout.write(splitData[splitData.length - 1]);
-    });
   });
+
+  client.on('data', (data) => {
+    const splitData = data.toString().split('\n\n');
+    responseHeaders.push(data.toString().split('\n\n')[0]);
+    process.stdout.write(splitData[splitData.length - 1]);
+  });
+};
+
+if (fullUrl.indexOf(':') > -1 && fullUrl.indexOf('/') > -1) {
+  var hostName = `http://${fullUrl.split(':')[0]}`;
+  var portNum = fullUrl.split(':')[1].split('/')[0];
+  var pathName = `/${fullUrl.split('/')[1]}`;
+
+  connectClient(hostName, portNum, pathName);
+
+} else if(fullUrl.indexOf(':') > -1) {
+  var hostName = `http://${fullUrl.split(':')[0]}`;
+  var portNum = fullUrl.split(':')[1];
+  var pathName = '/';
+
+  connectClient(hostName, portNum, pathName);
+
+} else if (fullUrl.indexOf('/') > -1) {
+  var hostName = `http://${fullUrl.split('/')[0]}`;
+  var pathName = `/${fullUrl.split('/')[1]}`;
+
+  connectClient(hostName, `80`, pathName);
+
+} else if (fullUrl !== undefined) {
+  var hostName = fullUrl;
+  var pathName = '/';
+
+  connectClient(hostName, 80, pathName);
+} else {
+  process.stdout.write(`Run client as such: 'node client.js hostname:port/path'`);
 }
