521f8f1f5a910af66409b9a00abe620f543bb864
GaganTut <gstut@live.com>
2017-04-13 19:40:14 -1000

    arguments work but small bug still

--- client.js
+++ client.js
@@ -1,16 +1,26 @@
 /*jshint esversion: 6*/
 const net = require('net');
-const path = process.argv[2];
+const fullUrl = process.argv[2];
 
-const client = net.connect({port: 8080}, () => {
-  if (path === undefined) {
-    process.stdout.write(`Type '/sample/sample' after client.js to run`);
-  } else {
-    client.write(`GET ${path} HTTP/1.1`);
-  }
+const hostName = fullUrl.split(":")[0];
+const portNum = fullUrl.split(":")[1].split("/")[0];
+const pathName = `/${fullUrl.split("/")[1]}`;
 
-  client.on('data', (data) => {
-    const splitData = data.toString().split('\n\n');
-    process.stdout.write(splitData[splitData.length - 1]);
+console.log(hostName, portNum, pathName);
+if (hostName === undefined) {
+  process.stdout.write(`Type path as such: "node client.js localhost:8080/sample"`);
+} else {
+  const client = net.connect({host: hostName, port: portNum}, () => {
+    if (pathName === undefined) {
+      process.stdout.write(`Type path as such: "node client.js localhost:8080/sample"`);
+    } else {
+      console.log(pathName.length);
+      client.write(`GET ${pathName} HTTP/1.1`);
+    }
+
+    client.on('data', (data) => {
+      const splitData = data.toString().split('\n\n');
+      process.stdout.write(splitData[splitData.length - 1]);
+    });
   });
-});
+}
